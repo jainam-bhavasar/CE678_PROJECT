4168bf2d4519b4f4e185a0fdfdc62dfa8d677811
Jainam-IITK <66310817+Jainam-IITK@users.noreply.github.com>
2020-12-07 09:44:29 +0530

    -

--- create_grid.m
+++ create_grid.m
@@ -1,4 +1,4 @@
-function [Latitude_grid,Longitude_grid] = create_grid(latitude,longitude,res)
+function [Latitude_grid,Longitude_grid] = create_grid(latitude,longitude,res,error)
 %create_grid(latitude,longitude,res) creates grid using latitude and
 %longitude vectors from the airborne gravity data
 %INPUTS
@@ -7,6 +7,8 @@ function [Latitude_grid,Longitude_grid] = create_grid(latitude,longitude,res)
 %2. longitude(double)-(degrees)Longitude vector acquired from the airborne
 %gravity data
 %3. res(double) - (degrees) spacing between 2 longitudes/spacing between 2
+%4 error(double) - (degrees) reduce range by error so that there are no NaN
+%values while interpolating delg.
 %latitudes
 %OUTPUT
 %[Latitude_grid,Longitude_grid] - grid formed by meshgridding vectors
@@ -14,8 +16,9 @@ function [Latitude_grid,Longitude_grid] = create_grid(latitude,longitude,res)
 %<a href="matlab:help import_airborne_gravity_data">Help for importing airborne data</a>
 %TIP- put same resolution that you took when downloading ggm and geoid data
 %from <a href="matlab:web('http://icgem.gfz-potsdam.de/calcgrid','-browser')">here</a>
-
-    lat = min(latitude):res:max(latitude);
-    long = min(longitude):res:max(longitude);
+    
+    lat = min(latitude)+error:res:max(latitude)-error;
+    long = min(longitude)+error:res:max(longitude)-error;
     [Latitude_grid,Longitude_grid] = meshgrid(lat,long);
+    
 end
